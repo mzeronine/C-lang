12ecc09582713ac1621788ef13f52b0271b61766
Meghan Vaze <vaze.2114100366@mujonline.edu.in>
2021-06-26 12:42:59 +0530

    commit

--- multiply.c
+++ multiply.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-int main
+int main()
 {
     int a, b, c;
 
     printf("Multiplication");
-    printf("Enter the num");
+    printf("Enter the num:");
     scanf("%d", &a);
-    printf("Enter the second num");
+    printf("Enter the second num:");
     scanf("%d", &b);
     c = a * b;
     printf("Multiplication of your num is: %d", c);
