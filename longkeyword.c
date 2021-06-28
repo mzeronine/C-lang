8db28dc2f10608ac2b91a466ce79ea98a39efc94
Meghan Vaze <vaze.2114100366@mujonline.edu.in>
2021-06-28 22:24:07 +0530

    Done

---
+++ longkeyword.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+    long l;
+    int i;
+    double d;
+    char c;
+
+    printf("The size of long: %zu \n", sizeof(l));
+    printf("The size of int: %zu \n", sizeof(i));
+    printf("The size of double: %zu \n", sizeof(d));
+    printf("The size of char: %zu ", sizeof(c));
+
+    return 0;
+
+}
