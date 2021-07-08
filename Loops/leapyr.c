d05b39eeb6961c2b462da917f016eee9464773c1
Meghan Vaze <vaze.2114100366@mujonline.edu.in>
2021-07-08 23:24:31 +0530

    Final

--- Loops/leapyr.c
+++ Loops/leapyr.c
@@ -1,18 +1,22 @@
-#include <stdio.h>
-int main() {
-   int year;
-   printf("Enter a year: ");
-   scanf("%d", &year);
-   
-   // leap year if not divisible by 100
-   // but divisible by 4
-   if (year % 4 == 0) {
-      printf("%d is a leap year.", year);
-   }
-   // all other years are not leap years
-   else {
-      printf("%d is not a leap year.", year);
-   }
+#include<stdio.h>
+int main()
+{
+    int year;
+    printf("Hello:\n");
+    printf("Please enter the year:");
+    scanf("%d", &year);
 
-   return 0;
+    if(year % 4==0)
+    {
+        printf("%d is leap year.", year);
+
+    }
+
+    else
+    {
+        printf("%d is not leap year.", year);
+    }
+
+    return 0;
+
 }
